3e752f77137bdab2dc03cd690a50771746ce0414
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2016-02-04 23:43:23 +0100

    add game generator

--- src/game.js
+++ src/game.js
@@ -1,17 +1,23 @@
 'use strict';
 
-function addVec2(a, b) {
-  var v1 = a[0] + b[0];
-  var v2 = a[1] + b[1];
-  return [v1, v2];
+function game(options) {
+  var velocity = options.velocity;
+
+  function getNextPhase(oldBall) {
+    return addVec2(oldBall, velocity);
+  }
+
+  function addVec2(a, b) {
+    var v1 = a[0] + b[0];
+    var v2 = a[1] + b[1];
+    return [v1, v2];
+  }
+
+  return {
+    getNextPhase: getNextPhase
+  };
 }
 
-function getNextPhase(oldBall) {
-  var velocity = [1, 1];
-  return addVec2(oldBall, velocity);
-}
 
-module.exports = {
-  getNextPhase: getNextPhase
-};
+module.exports = game;
 
--- test/game.spec.js
+++ test/game.spec.js
@@ -5,8 +5,24 @@ var game = require('../src/game');
 
 
 test(function(t) {
-  t.deepEqual(game.getNextPhase([100, 100]), [101, 101]);
+  t.deepEqual(generateGame([1, 1]).getNextPhase([100, 100]), [101, 101]);
 
-  t.deepEqual(game.getNextPhase([99, 99]), [100, 100]);
+  t.deepEqual(generateGame([1, 1]).getNextPhase([99, 99]), [100, 100]);
+
+  t.deepEqual(generateGame([2, 2]).getNextPhase([100, 100]), [102, 102]);
   t.end();
 });
+
+test(function(t) {
+  
+  t.deepEqual();
+
+  t.end();
+});
+
+function generateGame(velocity) {
+  return game({
+    velocity: velocity
+  });
+}
+
